ac12d18aa10c2d68334473f299b188f2bbda9c72
NehaMadhekar09 <124396613+NehaMadhekar09@users.noreply.github.com>
2023-03-11 23:26:15 -0500

    # Basic algorithm implemented with 4 actions

--- Project2_Dijkstras.py
+++ Project2_Dijkstras.py
@@ -1,6 +1,7 @@
 import pygame
 import numpy as np
- 
+import heapq as hq
+from collections import deque
 
 pygame.init()
 canvas=pygame.display.set_mode((600,250))
@@ -26,7 +27,7 @@ hex_points_inflated=[]
 line_offset=5
 offset_for_vertex=line_offset/np.cos(np.deg2rad(30))
 inflated_side= side + offset_for_vertex
-print(inflated_side)
+# print(inflated_side)
 
 for i in range(6):
     x=centre_hex[0]+side*np.cos(np.deg2rad(60*i + 30))
@@ -49,6 +50,144 @@ pygame.draw.polygon(canvas, (80,208,255), ((460,25),(460,225),(510,125)))
 pygame.draw.polygon(canvas, (80,208,255), ((460,25),(460,225),(510,125)),2)
 
 pygame.display.flip()
+# //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+# Checks whether the left move is posiible  
+def IsLeftMovePossible(current_node,closed_queue):
+    if not(current_node[3][0]==0):
+        new_node_xy=(current_node[3][0]-1,current_node[3][1])
+        if not(new_node_xy in list(zip(*closed_queue))[3]):
+            return[True,new_node_xy]
+    return[False]
+
+# Checks whether the right move is posiible  
+def IsRightMovePossible(current_node,closed_queue):
+    if not(current_node[3][0]==600):
+        new_node_xy=(current_node[3][0]+1,current_node[3][1])
+        if not(new_node_xy in list(zip(*closed_queue))[3]):
+            return[True,new_node_xy]
+    return[False]
+
+# Checks whether the up move is posiible  
+def IsUpMovePossible(current_node,closed_queue):
+    if not(current_node[3][1]==0):
+        new_node_xy=(current_node[3][0],current_node[3][1]-1)
+        if not(new_node_xy in list(zip(*closed_queue))[3]):
+            return[True,new_node_xy]
+    return[False]
+
+# Checks whether the down move is posiible  
+def IsDownMovePossible(current_node,closed_queue):
+    if not(current_node[3][1]==250):
+        new_node_xy=(current_node[3][0],current_node[3][1]+1)
+        if not(new_node_xy in list(zip(*closed_queue))[3]):
+            return[True,new_node_xy]
+    return[False]
+    
+
+def UpdateOpenList(current_node,new_node_xy,step_cost,open_queue,index):
+    cost_to_come=current_node[0] + step_cost
+    is_present=False
+    for i in range(len(open_queue)):
+                if open_queue[i][3]==new_node_xy:
+                    is_present=True
+                    if(cost_to_come < open_queue[i][0]):
+                        open_queue[i][0]=cost_to_come
+                        open_queue[i][2]=current_node[1]
+                        open_queue[i][3]=new_node_xy
+                    new_node=open_queue[i]
+    if not is_present:
+        new_node=(cost_to_come,index,current_node[1],new_node_xy)
+        hq.heappush(open_queue,new_node)
+    hq.heapify(open_queue)
+    return new_node
+
+# This function generates path by backtracking.
+def BacktrackPath(closed_queue,goal_xy,goal_parent_index):
+    path=deque()
+    path.appendleft(goal_xy)
+    parent_index=goal_parent_index
+    index=1
+    while(index!=0):
+        for i in range(len(closed_queue)):
+            if(closed_queue[i][1]==parent_index):
+                path.appendleft(closed_queue[i][3])
+                parent_index=closed_queue[i][2]
+                index=closed_queue[i][1]
+    return path
+
+# Dijkstras Algorithm
+
+def DijkstrasAlgorithm(start_node_xy, goal_node_xy):
+    open_Q = []
+    closed_Q=[]
+    
+    # creating tuple with cost to come, index, parent node index and coordinate values (x,y)
+    node=(0, 0, 0, start_node_xy)  
+    hq.heappush(open_Q, node)
+    hq.heapify(open_Q)
+    hq.heapify(closed_Q)
+
+    index=1
+    # Run for maximum 1 million iterations
+    while index < 1000000:
+        current_node=hq.heappop(open_Q)
+        hq.heappush(closed_Q,current_node)
+        hq.heapify(closed_Q)
+        hq.heapify(open_Q)
+
+        right_move=IsRightMovePossible(current_node,closed_Q)
+        if(right_move[0]):
+            new_node_xy=right_move[1]
+            new_node=UpdateOpenList(current_node,new_node_xy,1,open_Q,index)
+            index+=1
+            if(new_node_xy==goal_node_xy):
+                goal_parent_index=new_node[2]
+                break
+
+        up_move=IsUpMovePossible(current_node,closed_Q)
+        if(up_move[0]):
+            new_node_xy=up_move[1]
+            new_node=UpdateOpenList(current_node,new_node_xy,1,open_Q,index)
+            index+=1
+            if(new_node_xy==goal_node_xy):
+                goal_parent_index=new_node[2]
+                break
+
+        left_move=IsLeftMovePossible(current_node,closed_Q)
+        if(left_move[0]):
+            new_node_xy=left_move[1]
+            new_node=UpdateOpenList(current_node,new_node_xy,1,open_Q,index)
+            index+=1
+            if(new_node_xy==goal_node_xy):
+                goal_parent_index=new_node[2]
+                break
+        
+        down_move=IsDownMovePossible(current_node,closed_Q)
+        if(down_move[0]):
+            new_node_xy=down_move[1]
+            new_node=UpdateOpenList(current_node,new_node_xy,1,open_Q,index)
+            index+=1
+            if(new_node_xy==goal_node_xy):
+                goal_parent_index=new_node[2]
+                break
+
+        if(len(open_Q)==0):
+            print("Failed to find solution")
+            return
+
+
+    path=BacktrackPath(closed_Q,goal_node_xy,goal_parent_index)
+
+    return path
+
+
+
+
+
+path=DijkstrasAlgorithm((1,1),(10,10))
+print(path)
+
 
 running = True
   
